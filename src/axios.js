b92effafbcb402085c615ec43faf977fdfb63331
HenokTG <hena143877tg@gmail.com>
2022-09-28 17:48:47 +0300

    changed backendhost URL to new backend server

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // const baseURL = "http://127.0.0.1:8000/";
-const baseURL = "https://tabor-remit-backend.herokuapp.com/";
+// const baseURL = "https://tabor-remit-backend.herokuapp.com/";
+const baseURL = "https://hena-remit-backend.herokuapp.com/";
 
 export const axiosInstance = axios.create({
   baseURL,
